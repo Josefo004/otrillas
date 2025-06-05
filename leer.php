d5ced339442486022e8727c508a88607598075e4
José Mendoza V. <josemv004@gmail.com>
2025-06-05 19:28:29 -0400

    llevar

--- leer.php
+++ leer.php
@@ -31,83 +31,150 @@ function getSEA($codigo){
     return $re;
 }
 
-function verificarSEA($codSEA, $datos) {
-    $sea = getSEA($codSEA);
-
-    if(floatval($datos['Tarea:Notas de Práctica (Real)']) > $sea['practicas']) {
-        $datos['Practicas Rev. SEA'] = "practicas excel = ".$datos['Tarea:Notas de Práctica (Real)']." - practicasSEA = ".$sea['practicas'];
-    }
-
-    if(floatval($datos['Total Parcial']) > $sea['parciales']) {
-        $datos['Parciales Rev. SEA'] = "parciales excel = ".$datos['Total Parcial']." - parcialesSEA = ".$sea['parciales'];
-    }
-
-    if(floatval($datos['Cuestionario:EXAMEN FINAL (Real)']) > $sea['final']){
-        $datos['Final Rev. SEA'] = "final excel = ".$datos['Cuestionario:EXAMEN FINAL (Real)']." - finalSEA = ".$sea['final'];
-    }
-
-    if(floatval($datos['Cuestionario:EXAMEN FINAL (Real)']) + floatval($datos['Total Parcial']) + floatval($datos['Tarea:Notas de Práctica (Real)']) > 100){
-        $datos['Total Rev. SEA'] = "total excel = ".(floatval($datos['Cuestionario:EXAMEN FINAL (Real)']) + floatval($datos['Total Parcial']) + floatval($datos['Tarea:Notas de Práctica (Real)']))." - totalSEA = 100";
-    }
-
-    if (floatval($datos['Segunda Instancia (Real)']) > $sea['final']) {
-        $datos['Segunda Instancia Rev. SEA'] = "segunda instancia excel = ".$datos['Segunda Instancia (Real)']." - finalSEA = ".$sea['final'];
-    }
-    
-    return $datos;
-
-    
-}
-
-
 // leer.php
 function newArray($cabecera, $datos) {
     $narray = [];
     foreach ($cabecera as $key => $value) {
         $narray[trim($value)] = (trim($datos[$key]) === '') ? '0' : trim(str_replace(',', '.', $datos[$key]));
     }
+    $narray['errores'] = [];
     return $narray;
 }
 
-function promediotareas($datos) {
+function Revtareas($datos, $sea) {
     $sumaTareas = 0;
     $cantidadTareas = 0;
+    $practicas = $sea['practicas'];
 
     foreach ($datos as $clave => $valor) {
         // Detectamos tareas válidas (empiezan con "Tarea" y NO contienen ":")
         if (str_starts_with($clave, 'Tarea') && !str_contains($clave, ':')) {
-            $sumaTareas += floatval($valor);
+            $tareaX = floatval($valor);
+            if ($tareaX > $practicas) {
+                $datos['errores'][] = ['Tipo' => 'Tarea', 'campo'=>$clave, 'valorEXCEL'=>$valor, 'valorSEA'=> $practicas, 'msg'=>'la nota del campo '.$clave.' es mayor al SEA'];
+            }
+            $sumaTareas += $tareaX;
             $cantidadTareas++;
+
         }
     }
+
     // Calcular el promedio si hay tareas
     $promedioTareas = $cantidadTareas > 0 ? round($sumaTareas / $cantidadTareas, 2) : 0;
 
-    // Agregamos el nuevo cálculo al array
-    $datos['Tarea:Notas de Práctica (Real) Calculado'] = $promedioTareas;
-    $datos['Dif. Tarea:Notas de Práctica (Real)'] = floatval($datos['Tarea:Notas de Práctica (Real)']) - $promedioTareas;
+    if ($promedioTareas > $practicas) {
+        $datos['errores'][] = ['Tipo'=>'Tarea Promedio', 'campo' =>'Tarea:Notas de Práctica (Real)', 'valorEXCEL'=>$datos['Tarea:Notas de Práctica (Real)'], 'ValorCalculado' =>$promedioTareas, 'valorSEA'=> $practicas, 'msg'=>'el promedio de tareas es mayor al SEA'];
+    }
+
+    if ($promedioTareas - floatval($datos['Tarea:Notas de Práctica (Real)']) != 0) {
+        $datos['errores'][] = ['Tipo'=>'Tarea Promedio', 'campo' =>'Tarea:Notas de Práctica (Real)', 'valorEXCEL'=>$datos['Tarea:Notas de Práctica (Real)'], 'ValorCalculado' =>$promedioTareas, 'msg'=>'Hay diferencia entre Tarea:Notas de Práctica (Real) y su valor calculado'];
+    }
+    
     return $datos;
 }
 
-function promedioParciales($datos) {
+function promedioParciales($datos, $sea) {
     $sumaParciales = 0;
     $cantidadParciales = 0;
+
 
     foreach ($datos as $clave => $valor) {
         // Detectamos parciales válidos (empiezan con "Parcial" y NO contienen ":")
         $nClave = strtoupper($clave);
         if (strpos($nClave, 'PARCIAL (REAL)') !== false){
-            $sumaParciales += floatval($valor);
+            $parcialX = floatval($valor);
+            if ($parcialX > $sea['parciales']) {
+                $datos['errores'][] = ['Tipo' => 'Parcial', 'campo'=>$clave, 'valorEXCEL'=>$valor, 'valorSEA'=> $sea['parciales'], 'msg'=>'la nota del campo '.$clave.' es mayor al SEA'];
+            }
+            $sumaParciales += $parcialX;
             $cantidadParciales++;
         }
     }
     // Calcular el promedio si hay parciales
     $promedioParciales = $cantidadParciales > 0 ? round($sumaParciales / $cantidadParciales, 2) : 0;
-
-    // Agregamos el nuevo cálculo al array
-    $datos['Total Parcial Calculado'] = $promedioParciales;
-    $datos['Dif. Total Parcial Calculado'] = floatval($datos['Total Parcial']) - $promedioParciales;
+    if ($promedioParciales > $sea['parciales']) {
+        $datos['errores'][] = ['Tipo'=>'Parcial Promedio', 'campo' =>'Total Parcial', 'valorEXCEL'=>$datos['Total Parcial'], 'ValorCalculado' =>$promedioParciales, 'valorSEA'=> $sea['parciales'], 'msg'=>'el promedio de parciales es mayor al SEA'];
+    }
+    if ($promedioParciales - floatval($datos['Total Parcial']) != 0) {
+        $datos['errores'][] = ['Tipo'=>'Parcial Promedio', 'campo' =>'Total Parcial', 'valorEXCEL'=>$datos['Total Parcial'], 'ValorCalculado' =>$promedioParciales, 'msg'=>'Hay diferencia entre Total Parcial y su valor calculado'];
+    }
+    
     return $datos;
+}
+
+function generarSQL($datos) {
+    $Ci = $datos['Nombre de usuario'];
+    $CodigoCarrera = '127';
+    $NumeroPlanEstudios = '1';
+    $GestionAcademica = '2025';
+    $CodigoModalidadCurso = 'NA';
+    $SiglaMateria = 'CJS102';
+    $CodigoSEA = $datos['SEA'];
+    $Grupo = $datos['Grupo'];
+
+    $NotasPracticas = $datos['Tarea:Notas de Práctica (Real)'];
+    $PromedioParciales = $datos['Total Parcial'];
+    $NotaSemifinal = $datos['Nota Entrada'];
+    $ExamenFinal = $datos['Cuestionario:EXAMEN FINAL (Real)'];
+    $NotaFinal = $datos['Nota Final (Real)'];
+    $ExamenSegundaInstancia = $datos['Segunda Instancia (Real)'];    
+    $NotaSegundaInstancia = $datos['Notas Finales (Segunda Instancia)'];
+    
+    
+    $tareas = [];
+    $campos_tarea = ['Tarea1', 'Tarea2', 'Tarea3', 'Tarea4', 'Tarea5', 'Tarea6', 'Tarea7', 'Tarea8', 'Tarea9', 'Tarea10'];
+    $k=0;
+    foreach ($datos as $clave => $valor) {
+        if (str_starts_with($clave, 'Tarea') && !str_contains($clave, ':')) {
+            $tareas[$campos_tarea[$k]] = $valor;
+            $k++;
+        }
+    }
+
+    $parciales = [];
+    $campos_parcial = ['Parcial1', 'Parcial2', 'Parcial3'];
+    $k=0;
+    foreach ($datos as $clave => $valor) {
+        // Detectamos parciales válidos (empiezan con "Parcial" y NO contienen ":")
+        $nClave = strtoupper($clave);
+        if (strpos($nClave, 'PARCIAL (REAL)') !== false){
+            $parciales[$campos_parcial[$k]] = $valor;
+            $k++;
+        }
+    }
+
+    echo '<pre>'.var_export($datos, true).'</pre>';
+    echo '<pre>'.var_export($tareas, true).'</pre>';
+    echo '<pre>'.var_export($parciales, true).'</pre>';
+
+    $q = "INSERT INTO ImportarMoodleCalificaciones (Ci, CodigoCarrera, NumeroPlanEstudios, GestionAcademica, CodigoModalidadCurso, SiglaMateria, CodigoSEA, Grupo, ";
+    foreach($tareas as $clave => $valor) {$q .= "$clave, ";}
+
+    $q .= "NotasPracticas, ";
+    foreach($parciales as $clave => $valor) {$q .= "$clave, ";}
+
+    
+    $q .= "PromedioParciales, NotaSemifinal, ExamenFinal, NotaFinal, ExamenSegundaInstancia, NotaSegundaInstancia, FechaHoraImportar) ";
+    $q .= "VALUES ('$Ci', '$CodigoCarrera', '$NumeroPlanEstudios', '$GestionAcademica', '$CodigoModalidadCurso', '$SiglaMateria', '$CodigoSEA', '$Grupo', ";
+    foreach($tareas as $clave => $valor) {
+        $q .= "'" . $valor[$clave] . "', ";
+    }
+    echo $q . "\n<br>";
+    $q .= "'$NotasPracticas', ";
+    foreach($parciales as $clave => $valor) {
+        $q .= "'" . $valor[key($valor)] . "', ";
+    }
+    $q .= "'$PromedioParciales', '$NotaSemifinal', '$ExamenFinal', '$NotaFinal', '$ExamenSegundaInstancia', '$NotaSegundaInstancia', NOW())";
+
+
+
+    //Tarea1, Tarea2, Tarea3, Tarea4, Tarea5, Tarea6, Tarea7, Tarea8, Tarea9, Tarea10, NotasPracticas, Parcial1, Parcial2, Parcial3, PromedioParciales, NotaSemifinal, ExamenFinal, NotaFinal, ExamenSegundaInstancia, NotaSegundaInstancia, FechaHoraImportar) VALUES (";
+
+    // echo '<pre>'.var_export($tareas, true).'</pre>';
+    // echo '<pre>'.var_export($parciales, true).'</pre>';
+
+
+    echo $q . "\n<br>";
 }
 
 $archivo = fopen("CSV/dato02.csv", "r");
@@ -122,15 +189,68 @@ if ($archivo) {
             $cabecera = $narray; // Guardar la cabecera
         } else {
             $nuevoArray = newArray($cabecera, $narray);
-            $nuevoArray = promediotareas($nuevoArray); // Calcular el promedio de tareas
-            $nuevoArray = promedioParciales($nuevoArray); // Calcular el promedio de parciales
-            $nuevoArray = verificarSEA($nuevoArray['SEA'], $nuevoArray); // Verificar SEA
+            //echo '<pre>'.var_export($nuevoArray, true).'</pre>';
             $resultado[] = $nuevoArray; // Agregar el nuevo array al resultado
         }
         $k++;
     }
     fclose($archivo);
-    echo '<pre>'.var_export($resultado, true).'</pre>';
+
+    if (count($resultado) > 0) {
+        $sea = getSEA($resultado[0]['SEA']);
+        
+        foreach ($resultado as $key => &$value) {
+            $value = Revtareas($value, $sea); // Calcular el promedio de tareas
+            $value = promedioParciales($value, $sea); // Calcular el promedio de parciales
+        }
+
+    }
+
+    $estudiantes_d = array_values(array_filter($resultado, function($item) {
+        return (is_array($item['errores']) && count($item['errores']) > 0); // Filtrar solo los que tienen errores
+    }));
+
+    $estudiantes_d = array_map(function($item) {
+        return [
+            'Cu' => trim($item['Cu']),
+            'NombreUniversitario' => trim($item['NombreUniversitario']),
+            'CodigoCarrera' => trim($item['CodigoCarrera']),
+            'GestionAcademica' => trim($item['GestionAcademica']),
+            'CodigoModalidadCurso' => trim($item['CodigoModalidadCurso']),
+            'SiglaMateria' => trim($item['SiglaMateria']),
+            'errores' => is_array($item['errores']) ? implode(' | ', array_map(fn($e) => $e['msg'], $item['errores'])) : '', // Concatenamos los mensajes de error
+            'erroresJson' => json_encode($item['errores'], JSON_UNESCAPED_UNICODE) // Exportamos como JSON
+        ];
+    }, $estudiantes_d);
+
+    if (!empty($estudiantes_d)) {
+        echo '<pre>'.var_export($estudiantes_d, true).'</pre>';
+        // $nombreArchivo = "web/$SiglaMateria-$CodigoCarrera-errores_calificaciones-" . date('Ymd_His') . ".csv";
+
+        // echo "Generando archivo: $nombreArchivo\n";
+        // $fp = fopen($nombreArchivo, 'w');
+        // if ($fp === false) {
+        //     echo "Error al crear el archivo.\n";
+        //     return;
+        // }
+        // // Escribir encabezados
+        // fputcsv($fp, ['Cu', 'NombreUniversitario', 'CodigoCarrera', 'GestionAcademica', 'CodigoModalidadCurso', 'SiglaMateria', 'Errores', 'ErroresJson']);
+        // // Escribir datos
+        // foreach ($estudiantes_d as $fila) {
+        //     fputcsv($fp, $fila);
+        // }
+        // fclose($fp);
+        // echo "Archivo generado exitosamente: $nombreArchivo\n";
+    }
+    else {
+        echo "No se encontraron errores en las calificaciones.\n <br>";
+        //generar sql
+        // echo '<pre>'.var_export($resultado, true).'</pre>';
+        foreach ($resultado as $datos) {
+            generarSQL($datos);
+        }
+    }
+
 } else {
     echo "Error al abrir el archivo.";
 }
